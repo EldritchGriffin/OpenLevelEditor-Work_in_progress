e8a41a9fd43719d9154b392f108c48c462d419c9
EldritchGriffin <abdelhak.form@gmail.com>
2024-02-21 17:01:36 +0100

    added lines

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,24 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <rlgl.h>
+#include <vector>
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+
+class circle
+{
+public:
+    Vector2 position;
+    float radius;
+    Color color;
+    circle(Vector2 position, float radius, Color color) : position(position), radius(radius), color(color) {}
+    void draw()
+    {
+        DrawCircleV(position, radius, color);
+    }
+};
 
 void DrawGrid(Camera2D camera)
 {
@@ -29,8 +43,8 @@ int main()
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 640;
+    const int screenHeight = 400;
     Vector2 mousePos = {0.0f, 0.0f};
 
     InitWindow(screenWidth, screenHeight, "SimpleLevelEditor");
@@ -38,55 +52,70 @@ int main()
     Camera2D camera = {0};
     camera.zoom = 1.0f;
     camera.target = (Vector2){0.0f, 0.0f};
+    // camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
     // Main game loop
+    std::vector<circle> circles;
     while (!WindowShouldClose())
     {
         mousePos = GetMousePosition();
         Vector2 worldMousePos = GetScreenToWorld2D(mousePos, camera);
         const int gridSize = 40;
         Vector2 snappedPosition;
-        snappedPosition.x = (int)(worldMousePos.x / gridSize) * gridSize;
-        snappedPosition.y = (int)(worldMousePos.y / gridSize) * gridSize;
+        int biasx = worldMousePos.x >= 0 ? gridSize / 2 : -gridSize / 2;
+        int biasy = worldMousePos.y >= 0 ? gridSize / 2 : -gridSize / 2;
+        snappedPosition.x = (int)((worldMousePos.x + biasx) / gridSize) * gridSize;
+        snappedPosition.y = (int)((worldMousePos.y + biasy) / gridSize) * gridSize;
         // move the camera
         if (IsKeyDown(KEY_W))
         {
-            camera.target.y -= 10;
+            camera.offset.y += 10;
         }
         if (IsKeyDown(KEY_S))
         {
-            camera.target.y += 10;
+            camera.offset.y -= 10;
         }
         if (IsKeyDown(KEY_A))
         {
-            camera.target.x -= 10;
+            camera.offset.x += 10;
         }
         if (IsKeyDown(KEY_D))
         {
-            camera.target.x += 10;
+            camera.offset.x -= 10;
         }
         // draw a circle that snaps to the grid based on the mouse position in the world even if the grid isnt aligned with the screen without using predifined functions
-        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
             // convert snapped position to world position
             Vector2 worldSnappedPosition = GetWorldToScreen2D(snappedPosition, camera);
-            DrawCircleV(worldSnappedPosition, 5, BLUE);
+            circles.push_back(circle(worldSnappedPosition, 5, BLUE));
         }
-
         BeginDrawing();
         ClearBackground(DARKGRAY);
         BeginMode2D(camera);
         DrawGrid(camera);
+        for (auto c : circles)
+        {
+            c.draw();
+            // draw a line connecting the circles
+            if (circles.size() > 1)
+            {
+                for (int i = 0; i < circles.size() - 1; i++)
+                {
+                    DrawLineV(circles[i].position, circles[i + 1].position, WHITE);
+                }
+            }
+        }
         EndMode2D();
         EndDrawing();
     }
 
     // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow(); // Close window and OpenGL context
+
+    CloseWindow();
     //--------------------------------------------------------------------------------------
     return 0;
 }
