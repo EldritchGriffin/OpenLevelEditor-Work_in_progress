9f6f33ba0bc9bad0b9563f9641c6f44a430a07f8
EldritchGriffin <eldritchgriffin@Bocals-iMac.local>
2024-02-24 21:59:46 +0100

    working on editor layout

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,35 @@
 #include <sceneManager.hpp>
 #include <scene.hpp>
 
+std::string getSceneNames(SceneManager & sceneManager)
+{
+    std::vector<Scene *> scenes = sceneManager.getScenes();
+    std::string sceneNames = "";
+    for(auto scene : scenes)
+    {
+        sceneNames = sceneNames + scene->getName() + ";";
+    }
+    sceneNames = sceneNames.substr(0, sceneNames.size() - 1);
+    return (sceneNames);
+}
+
 void drawGui(SceneManager &sceneManager)
 {
+    static bool Button001Pressed = false;
+    static bool Button002Pressed = false;
+    static bool Button003Pressed = false;
+    static int ListView005ScrollIndex = 0;
+    static int ListView005Active = 0;
+    std::string sceneNames = getSceneNames(sceneManager);
+    ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR))); 
+
+    GuiGroupBox((Rectangle){ 24, 24, 288, 56 }, "Project Manager");
+    Button001Pressed = GuiButton((Rectangle){ 40, 40, 72, 24 }, "#008#New"); 
+    Button002Pressed = GuiButton((Rectangle){ 128, 40, 72, 24 }, "#004#Load"); 
+    Button003Pressed = GuiButton((Rectangle){ 216, 40, 72, 24 }, "#002#Save"); 
+    GuiGroupBox((Rectangle){ 24, 96, 288, 768 }, "Scene Manager");
+    GuiListView((Rectangle){ 72, 168, 192, 176 }, sceneNames.c_str(), &ListView005ScrollIndex, &ListView005Active);
+    GuiLabel((Rectangle){ 72, 144, 120, 24 }, "Scenes:");
 }
 
 int main()
@@ -17,6 +44,7 @@ int main()
     const int screenHeight = 800;
     SceneManager sceneManager;
     InitWindow(screenWidth, screenHeight, "SimpleLevelEditor");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
     try
     {
@@ -24,12 +52,12 @@ int main()
         sceneManager.createScene("scene2");
         sceneManager.createScene("scene3");
         sceneManager.SetActiveScene("scene1");
+        GuiLoadStyle("../styles/dark/style_dark.rgs");
         while (!WindowShouldClose())
         {
             sceneManager.Update();
             BeginDrawing();
             drawGui(sceneManager);
-            ClearBackground(RAYWHITE);
             sceneManager.Draw();
             EndDrawing();
         }
